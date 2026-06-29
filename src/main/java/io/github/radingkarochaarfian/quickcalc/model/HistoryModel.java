617f167cad2c52fc016f0c4bd7ed3ae9fb8f61f8
radingkarochaarfian <radingka.arfian@gmail.com>
2026-06-29 14:01:18 +0700

    add historymodel structure

--- src/main/java/io/github/radingkarochaarfian/quickcalc/model/HistoryModel.java
+++ src/main/java/io/github/radingkarochaarfian/quickcalc/model/HistoryModel.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class HistoryModel {
   public static class HistoryEntry {
-    private final String id;
+    private final int id;
     private final String expression;
     private final String result;
     private final List<String> listToken;
 
-    public HistoryEntry(String id, String expression, String result, List<String> listToken) {
+    public HistoryEntry(int id, String expression, String result, List<String> listToken) {
       this.id = id;
       this.expression = expression;
       this.result = result;
       this.listToken = new ArrayList<>(listToken);
     }
 
-    public String getId() {
+    public int getId() {
       return id;
     }
 
@@ -37,5 +37,36 @@ public class HistoryModel {
     public String getDisplayString() {
       return expression + "=" + result;
     }
+  }
+
+  private final List<HistoryEntry> listHistoryEntry = new ArrayList<>();
+  private int idCount;
+
+  public void addHistory(String expression, String result, List<String> listToken) {
+    HistoryEntry newEntry = new HistoryEntry(idCount++, expression, result, listToken);
+    listHistoryEntry.add(newEntry);
+  }
+
+  public List<String> getListTokenAt(int rowTableIndex) {
+    if (rowTableIndex >= 0 && rowTableIndex < listHistoryEntry.size()) {
+      return listHistoryEntry.get(rowTableIndex).listToken;
+    }
+    return new ArrayList<>();
+  }
+
+  public HistoryEntry getHistoryEntryAt(int rowTableIndex) {
+    if (rowTableIndex >= 0 && rowTableIndex < listHistoryEntry.size()) {
+      return listHistoryEntry.get(rowTableIndex);
+    }
+    return null;
+  }
+
+  public int getListHistoryEntrySize() {
+    return listHistoryEntry.size();
+  }
+
+  public void clearListHistoryEntry() {
+    listHistoryEntry.clear();
+    idCount = 1;
   }
 }
