63d0d21142534d4b0753ca0b43cee541efaf02a3
radingkarochaarfian <radingka.arfian@gmail.com>
2026-07-06 14:25:45 +0700

    reconstruct databaseconfig class to increase project capability

--- src/main/java/io/github/radingkarochaarfian/quickcalc/config/DatabaseConfig.java
+++ src/main/java/io/github/radingkarochaarfian/quickcalc/config/DatabaseConfig.java
@@ -15,20 +15,28 @@ public class DatabaseConfig {
   private final Properties prop;
   private static final String CONFIG_FILE_NAME = "db_config.properties";
 
+  public static final String KEY_PROVIDER_CLASS = "db.provider-class";
+  public static final String KEY_URL = "db.url";
+  public static final String KEY_MASTER_URL = "db.master-url";
+  public static final String KEY_NAME = "db.name";
+  public static final String KEY_USERNAME = "db.username";
+  public static final String KEY_PASSWORD = "db.password";
+  public static final String KEY_USE_LOCAL = "db.use.local";
+
   public DatabaseConfig() {
     prop = new Properties();
     loadConfirationFromProperties();
   }
 
   private void setDefaultValue() {
-    prop.setProperty("db.provider.class", "io.github.radingkarochaarfian.quickcalc.config.SqlServerProvider");
-    prop.setProperty("db.driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-    prop.setProperty("db.url",
+    prop.setProperty(KEY_PROVIDER_CLASS, "io.github.radingkarochaarfian.quickcalc.config.SqlServerProvider");
+    prop.setProperty(KEY_URL,
         "jdbc:sqlserver://localhost:1433;databaseName=QuickCalcDB;encrypt=true;trustServerCertificate=true;");
-    prop.setProperty("db.masterUrl", "jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true;");
-    prop.setProperty("db.name", "QuickCalcDB");
-    prop.setProperty("db.username", "sa");
-    prop.setProperty("db.password", "insertPassword");
+    prop.setProperty(KEY_MASTER_URL, "jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true;");
+    prop.setProperty(KEY_NAME, "QuickCalcDB");
+    prop.setProperty(KEY_USERNAME, "sa");
+    prop.setProperty(KEY_PASSWORD, "insertPassword");
+    prop.setProperty(KEY_USE_LOCAL, "false");
   }
 
   private void loadConfirationFromProperties() {
@@ -49,54 +57,61 @@ public class DatabaseConfig {
   }
 
   public String getProviderClass() {
-    return prop.getProperty("db.provider.class");
-  }
-
-  public String getDriverClass() {
-    return prop.getProperty("db.driver");
+    return prop.getProperty(KEY_PROVIDER_CLASS);
   }
 
   public String getUrl() {
-    return prop.getProperty("db.url");
+    return prop.getProperty(KEY_URL);
   }
 
   public String getMasterUrl() {
-    return prop.getProperty("db.masterUrl");
+    return prop.getProperty(KEY_MASTER_URL);
   }
 
   public String getDatabaseName() {
-    return prop.getProperty("db.name");
+    return prop.getProperty(KEY_NAME);
   }
 
   public String getUsername() {
-    return prop.getProperty("db.username");
+    return prop.getProperty(KEY_USERNAME);
   }
 
   public String getPassword() {
-    return prop.getProperty("db.password");
+    return prop.getProperty(KEY_PASSWORD);
   }
 
   public void setUsername(String username) {
-    prop.setProperty("db.username", username);
+    prop.setProperty(KEY_USERNAME, username);
   }
 
   public void setPassword(String password) {
-    prop.setProperty("db.password", password);
+    prop.setProperty(KEY_PASSWORD, password);
   }
 
-  public Connection getConnection() throws SQLException, ClassNotFoundException {
-    Class.forName(getDriverClass());
+  public boolean isUseLocalOnly() {
+    return Boolean.parseBoolean(prop.getProperty(KEY_USE_LOCAL));
+  }
+
+  public void setUseLocalOnly(boolean useLocal) {
+    prop.setProperty(KEY_USE_LOCAL, String.valueOf(useLocal));
+    savePropertiesToFile();
+  }
+
+  public Connection getConnection() throws SQLException {
     return DriverManager.getConnection(getUrl(), getUsername(), getPassword());
   }
 
-  public Connection getMasterConnection() throws SQLException, ClassNotFoundException {
-    Class.forName(getDriverClass());
+  public Connection getMasterConnection() throws SQLException {
     return DriverManager.getConnection(getMasterUrl(), getUsername(), getPassword());
   }
 
   public void ExportConfigToFile(String newUser, String newPass) {
     setUsername(newUser);
     setPassword(newPass);
+    savePropertiesToFile();
+  }
+
+  private void savePropertiesToFile() {
     try (FileOutputStream output = new FileOutputStream(CONFIG_FILE_NAME)) {
       prop.store(output, "QuickCalc Database Configuration (Auto-Generated)");
     } catch (IOException e) {
