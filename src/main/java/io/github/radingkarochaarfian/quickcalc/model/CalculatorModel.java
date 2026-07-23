88b02508e16c0c1604b462cd03cb6f763c99e60a
radingkarochaarfian <radingka.arfian@gmail.com>
2026-07-23 23:23:00 +0700

    add equation parser method to calculatormodel

--- src/main/java/io/github/radingkarochaarfian/quickcalc/model/CalculatorModel.java
+++ src/main/java/io/github/radingkarochaarfian/quickcalc/model/CalculatorModel.java
@@ -3,6 +3,8 @@ package io.github.radingkarochaarfian.quickcalc.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.github.radingkarochaarfian.quickcalc.util.CalculatorUtils;
+
 public class CalculatorModel {
   private List<String> listHistoryInput = new ArrayList<>();
   private int currIndex = -1;
@@ -68,6 +70,50 @@ public class CalculatorModel {
       sb.append(token);
     }
     return sb.toString();
+  }
+
+  public List<String> parseToInput(String equation) {// todo
+    List<String> listInput = new ArrayList<>();
+    if (equation == null || equation.trim().isEmpty())
+      return listInput;
+    char[] charEq = equation.toCharArray();
+    int len = charEq.length;
+    int i = 0;
+    while (i < len) {
+      char c = charEq[i];
+      if (Character.isWhitespace(c)) {
+        i++;
+        continue;
+      }
+      if (c == '(') {
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
+        while (i < len) {
+          sb.append(charEq[i]);
+          if (charEq[i] == '(')
+            open++;
+          else if (charEq[i] == ')')
+            open--;
+          i++;
+          if (open == 0)
+            break;
+        }
+        listInput.add(sb.toString());
+      } else if (Character.isDigit(c) || c == '.') {
+        StringBuilder sb = new StringBuilder();
+        while (i < len && (Character.isDigit(c) || charEq[i] == '.')) {
+          sb.append(charEq[i]);
+          i++;
+        }
+        listInput.add(sb.toString());
+      } else if (CalculatorUtils.isOperator(String.valueOf(c))) {
+        listInput.add(String.valueOf(c));
+        i++;
+      } else {
+        i++;
+      }
+    }
+    return listInput;
   }
 
   public String togglePlusMinusAt(String text, int caretPosition) {
